b927adb1af3fe2d7f0dc678d64db4546ec44bb61
Thusara Senanayake <89896736+thusarasenanayake@users.noreply.github.com>
2023-09-30 06:55:11 +0530

    add 1

---
+++ 1/src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import StopWatch from './StopWatch.jsx'
+import Table from './Table/Table.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  // <React.StrictMode>
+  // <App />
+  // <StopWatch />
+  <Table />
+  // </React.StrictMode>,
+)
